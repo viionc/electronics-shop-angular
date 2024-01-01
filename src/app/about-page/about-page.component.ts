e09ed5a544bc32cdca7a665f2499888584ef61d8
viionc <marek.d.stankiewicz@gmail.com>
2024-01-01 19:52:13 +0100

    feat: added about page

--- src/app/about-page/about-page.component.ts
+++ src/app/about-page/about-page.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../other-components/header/header.component';
 import { FooterComponent } from '../other-components/footer/footer.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-about-page',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    FooterComponent,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.css',
 })
